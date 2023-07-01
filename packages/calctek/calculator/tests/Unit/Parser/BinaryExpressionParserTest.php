0b8c65c5360d72e7a8670617260598e2a1ca1caa
Patrick Ratzow <patrickratzow@hotmail.dk>
2023-07-01 22:15:24 +0200

    test(calculator): added precedence tests for binary expressions

--- packages/calctek/calculator/tests/Unit/Parser/BinaryExpressionParserTest.php
+++ packages/calctek/calculator/tests/Unit/Parser/BinaryExpressionParserTest.php
@@ -5,13 +5,9 @@ namespace CalcTek\Calculator\Tests\Unit\Parser;
 use CalcTek\Calculator\Lexer\Token;
 use CalcTek\Calculator\Lexer\TokenType;
 use CalcTek\Calculator\Parser\Nodes\BinaryExpressionSyntaxNode;
-use CalcTek\Calculator\Parser\Nodes\CallExpressionSyntaxNode;
-use CalcTek\Calculator\Parser\Nodes\IdentifierSyntaxNode;
 use CalcTek\Calculator\Parser\Nodes\LiteralSyntaxNode;
-use CalcTek\Calculator\Parser\Nodes\UnaryExpressionSyntaxNode;
 use CalcTek\Calculator\Parser\Operator;
 use CalcTek\Calculator\Parser\Parser;
-use CalcTek\Calculator\Parser\SyntaxException;
 use Orchestra\Testbench\TestCase;
 
 class BinaryExpressionParserTest extends TestCase
@@ -37,6 +33,104 @@ class BinaryExpressionParserTest extends TestCase
                 Operator::Plus,
                 new LiteralSyntaxNode('2'),
                 new LiteralSyntaxNode('2')
+            ),
+            $ast
+        );
+    }
+
+    /** @test */
+    public function it_can_parse_a_binary_expression_with_precedence()
+    {
+        // Arrange
+        $input = collect([
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Operator, '+'),
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Operator, '*'),
+            new Token(TokenType::Literal, '5')
+        ]);
+        $parser = new Parser($input);
+
+        // Act
+        $parser->parse();
+        $ast = $parser->getAST();
+
+        // Assert
+        $this->assertEquals(
+            new BinaryExpressionSyntaxNode(
+                Operator::Plus,
+                new LiteralSyntaxNode('2'),
+                new BinaryExpressionSyntaxNode(
+                    Operator::Multiply,
+                    new LiteralSyntaxNode('2'),
+                    new LiteralSyntaxNode('5')
+                )
+            ),
+            $ast
+        );
+    }
+
+    /** @test */
+    public function it_can_parse_a_binary_expression_using_power_with_precedence()
+    {
+        // Arrange
+        $input = collect([
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Operator, '+'),
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Operator, '^'),
+            new Token(TokenType::Literal, '5')
+        ]);
+        $parser = new Parser($input);
+
+        // Act
+        $parser->parse();
+        $ast = $parser->getAST();
+
+        // Assert
+        $this->assertEquals(
+            new BinaryExpressionSyntaxNode(
+                Operator::Plus,
+                new LiteralSyntaxNode('2'),
+                new BinaryExpressionSyntaxNode(
+                    Operator::Power,
+                    new LiteralSyntaxNode('2'),
+                    new LiteralSyntaxNode('5')
+                )
+            ),
+            $ast
+        );
+    }
+
+    /** @test */
+    public function it_can_parse_a_binary_expression_with_correct_parentheses_precedence()
+    {
+        // Arrange
+        $input = collect([
+            new Token(TokenType::Separator, '('),
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Operator, '+'),
+            new Token(TokenType::Literal, '2'),
+            new Token(TokenType::Separator, ')'),
+            new Token(TokenType::Operator, '*'),
+            new Token(TokenType::Literal, '5')
+        ]);
+        $parser = new Parser($input);
+
+        // Act
+        $parser->parse();
+        $ast = $parser->getAST();
+
+        // Assert
+        $this->assertEquals(
+            new BinaryExpressionSyntaxNode(
+                Operator::Multiply,
+                new LiteralSyntaxNode('5'),
+                new BinaryExpressionSyntaxNode(
+                    Operator::Plus,
+                    new LiteralSyntaxNode('2'),
+                    new LiteralSyntaxNode('2')
+                )
             ),
             $ast
         );
