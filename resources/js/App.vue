48a3a6848276b4d2b8f651273485d2cdd6cef1fc
Patrick Ratzow <patrickratzow@hotmail.dk>
2023-07-03 02:07:09 +0200

    fix(ui): background colour not being fullscreen

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,7 +3,7 @@ import CalculatorPage from "./pages/CalculatorPage.vue";
 </script>
 
 <template>
-    <main class="bg-black md:bg-gray-200 h-full flex justify-center md:items-center p-4 md:p-0 pt-8 md:pt-4">
+    <main class="bg-black md:bg-gray-200 min-h-screen flex justify-center md:items-center p-4 pt-8 md:pt-4">
         <CalculatorPage />
     </main>
 </template>
